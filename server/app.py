f9881f5d0718582f430d6e1919ad6947dead9c66
Jonathan Grabowski <jon.grabowski@gmail.com>
2023-08-14 14:36:01 -0400

    set up react with flask template

---
+++ server/app.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# Standard library imports
+
+# Remote library imports
+from flask import request, make_response, session
+from flask_restful import Resource
+import ipdb
+
+# Local imports
+from config import app, db, api
+
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
