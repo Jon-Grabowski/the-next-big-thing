ec6b4feafd299ab038e0bb1bc34909fc6872c06f
Jonathan Grabowski <jon.grabowski@gmail.com>
2023-08-24 23:43:45 -0400

    tiny home page tweaks

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -7,12 +7,12 @@ function Home(){
                 <img 
                 src='../assets/thenextbigthing-high-resolution-logo-color-on-transparent-background.png'
                 alt='the-next-big-thing-logo'
-                className='w-25'
+                className='w-50'
                 />
             </div>
-            <div className='container-fluid my-3 w-100 pt-5'>
+            <div className='container-fluid my-1 w-100 pt-5'>
                 <div className='d-flex row justify-content-evenly justify-items-center align-items-center mt-5'>
-                    <div className='col-6 m-3'>
+                    <div className='col-5 m-1'>
                         <img 
                         // src='../assets/product-transparant.png'
                         src='../assets/nbt-display-og.jpg'
@@ -21,7 +21,7 @@ function Home(){
                         />
                     </div>
                     {/* <div className='col-1 m-1'></div> */}
-                    <div className='col-6 bg-black border border-white border-3 rounded bg-opacity-25 border-end-0 border-start-0 m-3 p-3'>
+                    <div className='col-5 bg-black border border-white border-3 rounded bg-opacity-25 border-end-0 border-start-0 m-0 p-3'>
                         <h5 className='text-light my-3 mb-5'>Introducing...    <span className='display-6 fst-italic fw-semibold'>theNextBigThing</span></h5>
                         <p className='text-light lead fst-italic my-3 text-center'>A true marvel that is set to reshape the way we experience the world. Prepare to embark on a journey into the future of technology, where innovation knows no bounds.</p>
                         <p className='text-light fs-5 my-3 text-center'>Imagine a world where limits are shattered and new horizons are unveiled. With theNextBigThing, you're not just witnessing progress - you're becoming a part of it. This extraordinary product embodies the culmination of relentless research, ingenious engineering, and visionary design.
