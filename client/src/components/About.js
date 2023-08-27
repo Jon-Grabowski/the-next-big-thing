af876eb2f537158447cc3515d952c58d9a4e6373
Jonathan Grabowski <jon.grabowski@gmail.com>
2023-08-27 16:39:32 -0400

    styled creator section

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -2,7 +2,10 @@
 function About() {
     return(
         <div className='container-lg text-light'>
-            <h1>About Page</h1>
+            <div className='d-flex align-items-end border-bottom mb-4'>
+                <p className='display-4'>Maximillian Veritas</p>
+                <p className='lead pb-1'><em>, creator & founder</em></p>
+            </div>
             <div className='d-flex align-items-center bg-black bg-opacity-50 pt-4 border-top border-4 rounded-top'>
                 <div className='float-start m-2'>
                     <img
@@ -13,9 +16,9 @@ function About() {
                     style = {{'max-width': '20rem'}}
                     />
                 </div>
-                <div className='align-middle'>
+                <div className='align-middle border-bottom border-1 mx-3'>
                     <p className='p-2 lead text-center fs-3'>
-                        <span classaName=''>The visionary mind behind theNextBigThing is an enigmatic figure who has etched an indelible mark on the tapestry of innovation. Emerging from the crossroads of brilliance and determination, their journey is a symphony of curiosity and relentless pursuit of the unknown. With a background steeped in multidisciplinary mastery, this inventor's life story reads like a mosaic of unconventional experiences and intellectual conquests.</span>
+                        <span classaName=''>Maximillian Veritas, the visionary mind behind theNextBigThing, is an enigmatic figure who has etched an indelible mark on the tapestry of innovation. Emerging from the crossroads of brilliance and determination, his journey is a symphony of curiosity and relentless pursuit of the unknown. With a background steeped in multidisciplinary mastery, Max's life story reads like a mosaic of unconventional experiences and intellectual conquests.</span>
                     </p>
                 </div>
                     
@@ -24,11 +27,9 @@ function About() {
                 </div>
             </div>
             <div className='bg-black bg-opacity-50 fs-5 p-4 border-bottom border-4 rounded-bottom'>
-                <p>
-                    From the earliest days, they displayed an uncanny knack for weaving together disparate threads of thought, transforming fragments of insight into the seeds of groundbreaking ideas. A background that spans across diverse realms – from quantum physics to esoteric art forms – granted them a unique vantage point from which to perceive unexplored possibilities.
-                    It was during an ethereal moment of serendipity that the spark of theNextBigThing was ignited. An encounter that defied the boundaries of conventional wisdom led to an epiphany that rippled through their consciousness, setting the course for an extraordinary creation. Fuelled by an insatiable curiosity and a desire to reshape the fabric of reality itself, they embarked on a clandestine journey to materialize their vision.
-                    Through countless sleepless nights and countless iterations, the inventor harnessed the arcane forces of innovation to sculpt theNextBigThing into existence. Guided by an innate connection to the pulse of tomorrow, they forged a path through uncharted territories of possibility. As the world waits with bated breath to uncover the shroud of secrecy, the inventor's legacy stands as a testament to the power of audacious dreams and the untamed spirit of exploration.
-                </p>
+                <p className='px-3'>From the earliest days, Maximillian displayed an uncanny knack for weaving together disparate threads of thought, transforming fragments of insight into the seeds of groundbreaking ideas. A background that spans across diverse realms - from quantum physics to esoteric art forms - granted him a unique vantage point from which to perceive unexplored possibilities.</p>
+                <p className='px-3'>It was during an ethereal moment of serendipity that the spark of theNextBigThing was ignited. An encounter that defied the boundaries of conventional wisdom led to an epiphany that rippled through his consciousness, setting the course for an extraordinary creation. Fuelled by an insatiable curiosity and a desire to reshape the fabric of reality itself, Max embarked on a clandestine journey to materialize his vision.</p>
+                <p className='px-3'>Through countless sleepless nights and countless iterations, Max harnessed the arcane forces of innovation to sculpt theNextBigThing into existence. Guided by an innate connection to the pulse of tomorrow, he forged a path through uncharted territories of possibility. As the world waits with bated breath for the release of theNextBigThing, Maximillian Veritas' legacy stands as a testament to the power of audacious dreams and the untamed spirit of exploration.</p>
             </div>
         </div>
     )
