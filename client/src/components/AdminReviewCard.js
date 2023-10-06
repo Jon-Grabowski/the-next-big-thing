9241951112a823b63f4034d2e4d8a6fc0ecf6810
Jonathan Grabowski <jon.grabowski@gmail.com>
2023-10-05 22:35:34 -0400

    added click to review cards

--- client/src/components/AdminReviewCard.js
+++ client/src/components/AdminReviewCard.js
@@ -1,10 +1,17 @@
+import { useState } from 'react'
 
 function AdminReviewCard({review}) {
     const {name, title, body, image} = review
+    const [cardTrigger, setCardTrigger] = useState(true)
+
+    function handleCardClick() {
+        setCardTrigger(!cardTrigger)
+    }
 
     return(
         <div>
-            <div className="card mb-3" style={{"max-width": "35rem"}}>
+            {cardTrigger ?
+            <div className="card mb-3" style={{"max-width": "35rem"}} onClick={handleCardClick}>
                 <div className="row g-0">
                     <div className="col-md-4 text-center justify-content-center my-4">
                     <img 
@@ -22,6 +29,18 @@ function AdminReviewCard({review}) {
                     </div>
                 </div>
             </div>
+            :
+            <div className="card mb-3" style={{"max-width": "35rem"}} onClick={handleCardClick}>
+                <div className="row g-0">
+                    <div className="col-md-8">
+                    <div className="card-body">
+                        <h5 className="card-title">{name}</h5>
+                        <p className="card-text">{body}</p>
+                        <p className="card-text"><small className="text-body-secondary">{title.toUpperCase()}</small></p>
+                    </div>
+                    </div>
+                </div>
+            </div>}
         </div>
     )
 }
